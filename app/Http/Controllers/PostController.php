2b3b9d74eafc521fc86e7f8c816b52514a9cdd7d
arjunstein <arjun.gnw09@gmail.com>
2023-08-08 13:54:29 +0700

    method and view insert data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,4 +22,30 @@ class PostController extends Controller
             ->column('action')
         ]);
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,jpg,png',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+         // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // insert new post to db
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'image' => $image->hashName(),
+        ]);
+
+        // render view
+        return redirect(route('posts.index'));
+    }
 }
